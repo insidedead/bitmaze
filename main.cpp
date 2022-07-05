3409f8f091e730ee2d3de10ae811cb8954bf104b
insidedead <nhocosousa@outlook.com>
2022-07-05 22:40:06 +0200

    sg

--- main.cpp
+++ main.cpp
@@ -4,20 +4,21 @@
 #define Hex(x) std::cout << std::hex << x;
 
 uint16_t map[6] = {
-	0, 	// 0000 0000 0000 0000
-	1 << 2, // 0000 0000 0000 0100
-	0, 	// 0000 0000 0000 0000
-       	0, 	// 0000 0000 0000 0000
-	0, 	// 0000 0000 0000 0000
-	1 << 11	// 0000 1000 0000 0000
+	0,
+	1 << 2,
+	0,
+       	0,
+	0,
+	1 << 11
 };
 
+#include <bitset>
 
 std::ostream& operator << (std::ostream &out, uint16_t *m)
 {
 	for(uint16_t i = 0; i < 6; i++)
 	{
-		out << std::hex << *(m + i) << std::endl;
+		out << std::hex << std::bitset<16>(*(m + i)) << std::endl;
 	}
 	return out;
 }
@@ -39,16 +40,16 @@ int main()
 			char pos {0};
 			uint16_t start = 0x8000;
 
-			while((start & 0xFFFF) != (map[index] & 0xFFFF))
+			while(!((start & 0xFFFF) & (map[index] & 0xFFFF)))
 			{
 				start >>=1;
 				++pos;
 			}
 
 			uint16_t left = map[index] << 1;
-			uint16_t right = map[index] << 1;
-			uint16_t top = map[index - 1] | (0x8000 >> pos); // todo
-			uint16_t bottom = map[index + 1] | map[index];
+			uint16_t right = map[index] >> 1;
+			uint16_t top = map[index - 1] | (0x8000 >> pos);
+			uint16_t bottom = map[index + 1] | (0x8000 >> pos);
 
 			if (left & map[index] || right & map[index] || top & map[index - 1] || bottom & map[index + 1]) target = true;
 
@@ -56,7 +57,8 @@ int main()
 			map[index - 1] |= top;
 			map[index + 1] |= bottom;
 
-			index = ((left | right) & 0xFFFF) ?  index : (top & 0xFFFF) ? top : bottom;
+			index = ((left | right) & 0xFFFF) ?  index : (top & 0xFFFF) ? index - 1 : index + 1;
+			pos = 0;
 		}
 	}
 
